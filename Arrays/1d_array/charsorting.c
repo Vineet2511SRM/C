bf360fc0ac3a486a857f5f079d4740b6ae092369
Vineet2511SRM <vs4622@srmist.edu.in>
2025-06-18 15:07:31 +0530

    C

--- Arrays/1d_array/charsorting.c
+++ Arrays/1d_array/charsorting.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int n;
     printf("Enter length of name (including spaces): ");
     scanf("%d", &n);
-    getchar();  // To consume the newline character after entering the length
+    getchar();  // Consume leftover newline
 
     char A[n + 1], temp;
 
     printf("Enter your name: ");
     fgets(A, sizeof(A), stdin);
 
-    // Selection sort algorithm
-    for (int sl = 0; sl < (n - 1); sl++) {
-        for (int cmp = (sl + 1); cmp < n; cmp++) {
-            if (A[sl] > A[cmp]) {  // Comparison
-                // Swap characters
+    // Remove newline character if exists
+    if (A[strlen(A) - 1] == '\n') {
+        A[strlen(A) - 1] = '\0';
+    }
+
+    int length = strlen(A);
+
+    // Selection sort
+    for (int sl = 0; sl < (length - 1); sl++) {
+        for (int cmp = sl + 1; cmp < length; cmp++) {
+            if (A[sl] > A[cmp]) {
                 temp = A[sl];
                 A[sl] = A[cmp];
                 A[cmp] = temp;
@@ -23,7 +30,7 @@ int main() {
         }
     }
 
-    printf("Sorted name is: %s ", A);
+    printf("Sorted name is: %s\n", A);
 
     return 0;
 }
